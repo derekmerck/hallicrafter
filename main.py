9e6e8cf909a280a4bfa529335236119ccd714250
derek <derek_merck@brown.edu>
2019-06-09 11:24:07 -0400

    Change config to panasonic

--- main.py
+++ main.py
@@ -6,39 +6,9 @@ from hallicrafter.pixels.renderers import FirePixelRenderer, RainbowPixelRendere
 from hallicrafter.sensor import AM2320Sensor, VEML7700Sensor
 from hallicrafter.oled import SSD1306
 
-"""
-config:
-  host: hallicrafter
-  
-  services:
-    lms_connection:
-      driver: LMSConnection
-      server_hostname: boxwood
-      player_name: hallicrafter
-      update: 5
-
-    backlight: 
-      driver: NeoPixels
-      num_pixels: 33
-      data_pin:  board.D12
-      
-    light_sensor:
-      driver: veml770
-      update: 0.1
-
-    temp_hum_sensor:
-      driver: am2320
-      update: 3
-      
-    oled_display:
-      driver: ssd1306
-      size: [128,64]
-      reset_pin: board.D4
-      
-"""
-
 pixel_pin = board.D12
-npixels = 33
+# npixels = 33  # hallicrafter
+npixels = 20  # panasonic
 oled_reset_pin = board.D4
 
 has_oled = True
@@ -55,18 +25,18 @@ if __name__ == "__main__":
 
     renderer = RainbowPixelRenderer(npixels=npixels)
     pixels = PixelStrip(pin=pixel_pin, npixels=npixels)
-    temp_hum_sensor = AM2320Sensor(delay=3)
-    light_sensor = VEML7700Sensor(delay=0.5)
-    oled = SSD1306()
+    # temp_hum_sensor = AM2320Sensor(delay=3)
+    # light_sensor = VEML7700Sensor(delay=0.5)
+    # oled = SSD1306()
 
-    oled.test()
+    # oled.test()
 
     while True:
 
         pixels.update()
-        temp_hum_sensor.update()
-        light_sensor.update()
-        oled.stats()
+        # temp_hum_sensor.update()
+        # light_sensor.update()
+        # oled.stats()
 
         sleep(loop_delay)
 

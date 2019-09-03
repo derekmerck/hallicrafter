b820a5a8dcb0b934fdbb0b579eec82d5c669656f
derek <derek_merck@brown.edu>
2019-09-02 21:47:24 -0400

    missing stuff in sys

--- hallicrafter2/device/power.py
+++ hallicrafter2/device/power.py
@@ -6,9 +6,8 @@ class Battery(Device):
     def __init__(self, name="bat0", interval=5.0, *args, **kwargs):
         Device.__init__(self, name=name, interval=interval, *args, **kwargs)
 
-        import analogio
-
         try:
+            import analogio
             self.voltage = analogio.AnalogIn(board.VOLTAGE_MONITOR)
         except:
             self.voltage = None
